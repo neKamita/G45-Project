347ad10a1dc5b375f8ec6eee22fdf25976776d14
cdy <nationalUB@gmail.com>
2025-01-08 11:55:50 +0500

    111

--- src/main/java/uz/pdp/config/RedisConfig.java
+++ src/main/java/uz/pdp/config/RedisConfig.java
@@ -1,25 +1,35 @@
 package uz.pdp.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.support.NoOpCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
 
     @Bean
-    @ConditionalOnMissingBean(RedisConnectionFactory.class)
-    public CacheManager cacheManager() {
+    @Primary
+    @ConditionalOnProperty(name = "spring.redis.enabled", havingValue = "false", matchIfMissing = true)
+    public CacheManager noCacheManager() {
         return new NoOpCacheManager();
     }
 
     @Bean
-    @ConditionalOnMissingBean(RedisConnectionFactory.class)
-    public RedisTemplate<String, Object> redisTemplate() {
-        return new RedisTemplate<>();
+    @ConditionalOnProperty(name = "spring.redis.enabled", havingValue = "true")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
     }
 }
