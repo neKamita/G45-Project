3f008cefcc68c945f7f448f8e94dcf28af9f7bf6
cdy <nationalUB@gmail.com>
2025-01-08 11:36:10 +0500

    Redis update

--- src/main/java/uz/pdp/config/RedisConfig.java
+++ src/main/java/uz/pdp/config/RedisConfig.java
@@ -1,32 +1,25 @@
 package uz.pdp.config;
 
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.support.NoOpCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-
-import java.time.Duration;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
-@EnableCaching
 public class RedisConfig {
 
     @Bean
-    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-            .entryTtl(Duration.ofHours(1))
-            .serializeValuesWith(
-                RedisSerializationContext.SerializationPair.fromSerializer(
-                    new GenericJackson2JsonRedisSerializer()
-                )
-            );
+    @ConditionalOnMissingBean(RedisConnectionFactory.class)
+    public CacheManager cacheManager() {
+        return new NoOpCacheManager();
+    }
 
-        return RedisCacheManager.builder(connectionFactory)
-            .cacheDefaults(config)
-            .build();
+    @Bean
+    @ConditionalOnMissingBean(RedisConnectionFactory.class)
+    public RedisTemplate<String, Object> redisTemplate() {
+        return new RedisTemplate<>();
     }
 }
