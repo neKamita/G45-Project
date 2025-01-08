2c2487b18cfe95d8bd6d3570f3acbc807220477d
cdy <nationalUB@gmail.com>
2025-01-08 14:03:21 +0500

    Disable Redis

--- src/main/java/uz/pdp/config/RedisConfig.java
+++ src/main/java/uz/pdp/config/RedisConfig.java
@@ -7,29 +7,15 @@ import org.springframework.cache.support.NoOpCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
+@ConditionalOnProperty(name = "spring.redis.enabled", havingValue = "false", matchIfMissing = true)
 public class RedisConfig {
 
     @Bean
     @Primary
-    @ConditionalOnProperty(name = "spring.redis.enabled", havingValue = "false", matchIfMissing = true)
-    public CacheManager noCacheManager() {
+    public CacheManager cacheManager() {
         return new NoOpCacheManager();
-    }
-
-    @Bean
-    @ConditionalOnProperty(name = "spring.redis.enabled", havingValue = "true")
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        return template;
     }
 }
