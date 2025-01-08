7e589a984224b93d09fff2b0c125c3c9730367ed
cdy <nationalUB@gmail.com>
2025-01-08 14:13:02 +0500

    Redis Claude Sytem ON

--- src/main/java/uz/pdp/config/RedisConfig.java
+++ src/main/java/uz/pdp/config/RedisConfig.java
@@ -7,11 +7,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.protocol.ProtocolVersion;
 
 import java.time.Duration;
 
@@ -21,7 +25,20 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName("redis-15073.crce175.eu-north-1-1.ec2.redns.redis-cloud.com");
+        redisConfig.setPort(15073);
+        redisConfig.setPassword("stdi3YBD24cdrO2zBd0oYrqXhl2xtWML");
+        redisConfig.setDatabase(0);
+        
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+            .clientOptions(ClientOptions.builder()
+                .protocolVersion(ProtocolVersion.RESP3)
+                .build())
+            .commandTimeout(Duration.ofSeconds(2))
+            .build();
+
+        return new LettuceConnectionFactory(redisConfig, clientConfig);
     }
 
     @Bean
@@ -36,7 +53,7 @@ public class RedisConfig {
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-            .entryTtl(Duration.ofHours(1))
+            .entryTtl(Duration.ofMinutes(30))
             .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
             .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
 
